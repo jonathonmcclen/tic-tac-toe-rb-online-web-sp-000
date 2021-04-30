bf6b09128a2691f3ec34cc7f1fe622cf1375b737
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-04-30 00:40:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,7 +51,7 @@ def play(board)
   
   amount_turns = 9
   the_turn = 0
-
+  who_won = current_player(board)
   while the_turn < amount_turns && !over?(board) 
     turn(board)
     who_won = current_player(board)
